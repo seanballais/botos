3360fa556170cd9a95efe109a7612888fbbbd1e0
Sean Francis N. Ballais <sean@seanballais.com>
2016-06-02 17:53:25 +0800

    Added a new __init__.py file for the app.

---
+++ app/__init__.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+from flask.ext.sqlalchemy import SQLAlchemy
+
+# Define the WSGI application object
+app = Flask(__name__)
+
+# Configurations
+app.config.from_object('config')
+
+# Define the database object which is imported
+# by modules and controllers
+db = SQLAlchemy(app)
+
+# Sample HTTP error handling
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('templates/default/404.html'), 404
+
+# Build the database
+db.create_all()
