1f01d43d12f4f35bfd6d41a48d2fc2fb722c41f0
Sean Francis N. Ballais <sean@seanballais.com>
2019-08-03 21:00:01 +0800

    Fixed bug where the user's password is not being hashed in the admin.

--- core/admin.py
+++ core/admin.py
@@ -1,12 +1,79 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import (
+    UserChangeForm, UserCreationForm
+)
+from django.contrib.auth.models import Group
+from django import forms
 
 from core.models import (
     User, Batch, Section, Candidate, CandidateParty, CandidatePosition
 )
 
-admin.site.register(User)
+
+# The following classes are based on the code by @kdh454 from:
+#    https://stackoverflow.com/a/17496836/1116098
+class CustomUserChangeForm(UserChangeForm):
+    class Meta(UserChangeForm.Meta):
+        model = User
+
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = User
+
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        try:
+            User.objects.get(username=username)
+        except User.DoesNotExist:
+            return username
+
+        raise forms.ValidationError(self.error_messages['duplicate_messages'])
+
+
+class CustomUserAdmin(UserAdmin):
+    form = CustomUserChangeForm
+    add_form = CustomUserCreationForm
+    fieldsets = (
+        (
+            None,
+            { 
+                'fields': (
+                    'username',
+                    'password',
+                    'first_name',
+                    'last_name',
+                    'email',
+                    'batch',
+                    'section',
+                )
+            } 
+        ),
+    )
+    add_fieldsets = (
+        (
+            None,
+            { 
+                'fields': (
+                    'username',
+                    'password1',
+                    'password2',
+                    'first_name',
+                    'last_name',
+                    'email',
+                    'batch',
+                    'section',
+                )
+            } 
+        ),
+    )
+
+
+admin.site.register(User, CustomUserAdmin)
 admin.site.register(Batch)
 admin.site.register(Section)
 admin.site.register(Candidate)
 admin.site.register(CandidateParty)
 admin.site.register(CandidatePosition)
+admin.site.unregister(Group)  # We don't need this at the moment.
