8e2b1aa19557161f0d4a76be1ecc35da079c181a
Sean Francis N. Ballais <sfballais123@gmail.com>
2020-05-04 18:10:49 +0800

    :construction_worker: Integrate Botos with Travis CI.

--- botos/settings/travis_settings.py
+++ botos/settings/travis_settings.py
@@ -1,10 +1,11 @@
-# If you don't have a pre-existing local_settings.py file,
-# duplicate this file and name it as local_settings.py. Fill
-# up the necessary values as necessary. You may remove this comment
-# header.
+"""
+This settings file is intended to be used with Travis CI. Feel free to use
+this as another template for your own local settings file.
+"""
+import os
 
 # Keep this a secret! In production, please change this to something else.
-SECRET_KEY = '6xtu*&k8@)pibt(^c2ox%@#3taw0zp_g05e28g!#0j3gjrf2!f'
+SECRET_KEY = 'T6xtu*&k8@)pibt(^c2ox%@#3taw0zp_g05e28g!#0j3gjrf2!RAVIS'
 
 # This should be True when in development. Only disable this in
 # staging/production.
@@ -19,10 +20,17 @@ DEBUG = True
 # your case.
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ['BOTOS_CI_DATABASE_NAME'],
+        'USER': os.environ['BOTOS_CI_DATABASE_USERNAME'],
+        'PASSWORD': '',
+        'TEST': {
+            'NAME': os.environ['BOTOS_CI_TEST_DATABASE_NAME']
+        }
     }
 }
+
+DATABASE_URL = os.environ['BOTOS_CI_DATABASE_URL']
 
 # In production, uncomment and set STATIC_ROOT to the template directory.
 # STATIC_ROOT = '/path/to/template/directory'
